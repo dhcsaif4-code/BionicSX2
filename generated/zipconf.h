c8e9dcae59e2032bdf3ac1a5273e029580d4e45d
dhcsaif4-code <dhcsaif4@gmail.com>
2026-05-15 17:16:56 +0000

    Fix: add zipconf.h stub for libzip

---
+++ generated/zipconf.h
@@ -0,0 +1,14 @@
+#pragma once
+/* Generated zipconf.h for BionicSX2 iOS port */
+#define LIBZIP_VERSION "1.11.2"
+#define LIBZIP_VERSION_MAJOR 1
+#define LIBZIP_VERSION_MINOR 11
+#define LIBZIP_VERSION_MICRO 2
+typedef signed char zip_int8_t;
+typedef unsigned char zip_uint8_t;
+typedef short zip_int16_t;
+typedef unsigned short zip_uint16_t;
+typedef int zip_int32_t;
+typedef unsigned int zip_uint32_t;
+typedef long long zip_int64_t;
+typedef unsigned long long zip_uint64_t;
