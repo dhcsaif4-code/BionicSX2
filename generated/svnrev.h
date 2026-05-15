7aafcf621be49d349fd53d62108f468564613a7a
dhcsaif4-code <dhcsaif4@gmail.com>
2026-05-15 16:55:53 +0000

    Fix: svnrev.h GIT_REV type, imgui include path
    GIT_REV must be string for const char*. GIT_TAGGED_COMMIT must
    be bool. imgui headers are in 3rdparty/imgui/include/ not root.

--- generated/svnrev.h
+++ generated/svnrev.h
@@ -7,10 +7,10 @@
 
 // Git version info used by BuildVersion.cpp
 #define GIT_TAG "v0.1.0"
-#define GIT_TAGGED_COMMIT "0000000000000000000000000000000000000000"
+#define GIT_TAGGED_COMMIT false
 #define GIT_TAG_HI 0
 #define GIT_TAG_MID 1
 #define GIT_TAG_LO 0
-#define GIT_REV 1
+#define GIT_REV "1"
 #define GIT_HASH "00000000"
 #define GIT_DATE __DATE__
