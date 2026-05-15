ce9aee16fe0801ad4193c939dcf33bfd9641c308
dhcsaif4-code <dhcsaif4@gmail.com>
2026-05-15 16:53:58 +0000

    Fix: svnrev.h git vars, imgui backend include path
    BuildVersion.cpp needs GIT_TAG/GIT_REV/GIT_HASH macros.
    ImGuiFullscreen.h needs imgui.h which is in 3rdparty/imgui

--- generated/svnrev.h
+++ generated/svnrev.h
@@ -4,3 +4,13 @@
 #define SVN_REVISION "0.1.0"
 #define SVN_DATE __DATE__
 #define SVN_TIME __TIME__
+
+// Git version info used by BuildVersion.cpp
+#define GIT_TAG "v0.1.0"
+#define GIT_TAGGED_COMMIT "0000000000000000000000000000000000000000"
+#define GIT_TAG_HI 0
+#define GIT_TAG_MID 1
+#define GIT_TAG_LO 0
+#define GIT_REV 1
+#define GIT_HASH "00000000"
+#define GIT_DATE __DATE__
