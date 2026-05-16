5005f408fa27689e2633a7857ad8187bf1fd1f1f
dhcsaif4-code <dhcsaif4@gmail.com>
2026-05-16 12:00:00 +0000

    Fix: add plutosvg_document_get_width/height/render stubs and plutovg_canvas_t typedef to plutosvg.h

--- generated/plutosvg.h
+++ generated/plutosvg.h
@@ -3,6 +3,7 @@
 
 typedef struct plutosvg_document_t plutosvg_document_t;
 typedef struct plutovg_surface_t plutovg_surface_t;
+typedef struct plutovg_canvas_t plutovg_canvas_t;
 
 #ifdef __cplusplus
 extern "C" {
@@ -24,6 +25,25 @@ static inline plutovg_surface_t* plutosvg_document_render_to_surface(
   plutovg_surface_t* surface) {
   (void)doc; (void)width; (void)height; (void)surface;
   return (plutovg_surface_t*)0;
+}
+
+static inline float plutosvg_document_get_width(
+  plutosvg_document_t* doc) {
+  (void)doc; return 0.0f;
+}
+
+static inline float plutosvg_document_get_height(
+  plutosvg_document_t* doc) {
+  (void)doc; return 0.0f;
+}
+
+static inline int plutosvg_document_render(
+  plutosvg_document_t* doc, const void* id,
+  plutovg_canvas_t* canvas, const void* matrix,
+  const void* clip_rect, const void* user_data) {
+  (void)doc; (void)id; (void)canvas;
+  (void)matrix; (void)clip_rect; (void)user_data;
+  return 0;
 }
 
 #ifdef __cplusplus
