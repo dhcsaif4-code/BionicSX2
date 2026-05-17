54ddd324da1c549cf4684b1f9e7402a07da03d5b
dhcsaif4-code <dhcsaif4@gmail.com>
2026-05-17 01:58:00 +0000

    fix(ui): add Metal render loop with CADisplayLink to fix black screen, add iPad Pro M4 support via UISupportedDeviceFamilies

--- ios/ui/MetalViewController.swift
+++ ios/ui/MetalViewController.swift
@@ -6,6 +6,7 @@ import QuartzCore
 
 class MetalViewController: UIViewController {
     var metalLayer: CAMetalLayer!
+    var displayLink: CADisplayLink!
 
     override class var layerClass: AnyClass { CAMetalLayer.self }
 
@@ -16,12 +17,17 @@ class MetalViewController: UIViewController {
         metalLayer.pixelFormat = .bgra8Unorm
         metalLayer.framebufferOnly = true
         metalLayer.frame = view.bounds
+        metalLayer.isOpaque = true
 
         // Start VM via C++ bridge
         let isoPath = findFirstISO()
         if !BionicSX2Bridge.startVM(isoPath: isoPath) {
             NSLog("[BionicSX2] iOSVMManager::StartVM failed")
         }
+
+        // Start render loop
+        displayLink = CADisplayLink(target: self, selector: #selector(renderFrame))
+        displayLink.add(to: .main, forMode: .common)
     }
 
     override func viewDidLayoutSubviews() {
@@ -31,6 +37,28 @@ class MetalViewController: UIViewController {
             width: view.bounds.width * view.contentScaleFactor,
             height: view.bounds.height * view.contentScaleFactor
         )
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        displayLink?.invalidate()
+    }
+
+    @objc func renderFrame() {
+        guard let drawable = metalLayer.nextDrawable() else { return }
+        let passDescriptor = MTLRenderPassDescriptor()
+        passDescriptor.colorAttachments[0].texture = drawable.texture
+        passDescriptor.colorAttachments[0].loadAction = .clear
+        passDescriptor.colorAttachments[0].clearColor = MTLClearColorMake(0, 0, 0, 1)
+
+        guard let commandQueue = metalLayer.device?.makeCommandQueue(),
+              let commandBuffer = commandQueue.makeCommandBuffer(),
+              let encoder = commandBuffer.makeRenderCommandEncoder(descriptor: passDescriptor) else {
+            return
+        }
+        encoder.endEncoding()
+        commandBuffer.present(drawable)
+        commandBuffer.commit()
     }
 
     private func findFirstISO() -> String? {
